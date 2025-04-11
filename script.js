5490c7db784a5d41c271437c02b74f146b093820
eduardo-luciano2 <eduardo.borowski.silva@escola.pr.gov.br>
2025-04-11 09:24:46 -0300

    kk

---
+++ script.js
@@ -0,0 +1,12 @@
+function hora(){
+    //relogio recebe a data atual
+    let relogio =- document.getElementById('relogio')
+    let tempo = new Date(); 
+    let horas = tempo.getHours();
+    let minutos = tempo.getMinutes();
+    let segundos = tempo.getSeconds();
+
+relogio.innerHTML
+   }
+   
+   hora();
